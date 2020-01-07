3159beb5722c802cb85682438cf6541516db25b6
Programazing <chris@thatamazingprogrammer.com>
2020-01-07 07:55:56 -0500

    Add Tip Calculator

---
+++ CodeWars/CodeWars/8kyu/TipCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars._8kyu
+{
+    public static class TipCalculator
+    {
+        public static int CalculateTip(double amount, string rating)
+        => TipRatings().ContainsKey(rating.ToLower()) ? (int)Math.Ceiling(TipRatings()[rating.ToLower()] * amount) : -1;
+
+        private static Dictionary<string, double> TipRatings()
+        {
+            return new Dictionary<string, double>
+            {
+                { "terrible", 0 },
+                { "poor", 0.05 },
+                { "good", 0.10 },
+                { "great", 0.15 },
+                { "excellent", 0.20 },
+            };
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/8kyu/TipCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars._8kyu;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWarsTests._8kyu
+{
+    [TestFixture]
+    public class TipCalculatorTests
+    {
+        [Theory]
+        [TestCase(20, "Excellent", 4)]
+        [TestCase(26.95, "good", 3)]
+        [TestCase(26.95, "meh", -1)]
+        [TestCase(23476, "TErriblE", 0)]
+        public void Test(double tip, string rating, int actual)
+        {
+            var result = TipCalculator.CalculateTip(tip, rating);
+
+            result.Should().Be(actual);
+        }
+    }
+}
