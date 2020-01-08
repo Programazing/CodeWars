b0dd93163f4fb6bb90c02fae903a0349b536301d
Programazing <chris@thatamazingprogrammer.com>
2020-01-08 11:13:09 -0500

    Break out requirements into separate tests.

--- CodeWars/CodeWarsTests/8kyu/TipCalculatorTests.cs
+++ CodeWars/CodeWarsTests/8kyu/TipCalculatorTests.cs
@@ -13,13 +13,23 @@ namespace CodeWarsTests._8kyu
         [Theory]
         [TestCase(20, "Excellent", 4)]
         [TestCase(26.95, "good", 3)]
-        [TestCase(26.95, "meh", -1)]
         [TestCase(23476, "TErriblE", 0)]
-        public void Test(double tip, string rating, int actual)
+        public void CalculateTip_Returns_ExpectedTip(double tip, string rating, int actual)
+        {
+            var result = TipCalculator.CalculateTip(tip, rating);
+
+            result.Should().Be(actual);
+        }
+
+        [Theory]
+        [TestCase(26.95, "meh", -1)]
+        [TestCase(20, "excellen", -1)]
+        public void CalculateTip_Returns_NegativeOne_WhenUnknownRatingIsPassedIn(double tip, string rating, int actual)
         {
             var result = TipCalculator.CalculateTip(tip, rating);
 
             result.Should().Be(actual);
         }
     }
+
 }
