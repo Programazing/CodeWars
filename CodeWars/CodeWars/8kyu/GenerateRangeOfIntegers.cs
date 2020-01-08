5b914aa9f78955ae265196315d19faed5edcb4cc
Programazing <chris@thatamazingprogrammer.com>
2020-01-08 11:12:39 -0500

    Move instructions from test class to main class.

--- CodeWars/CodeWars/8kyu/GenerateRangeOfIntegers.cs
+++ CodeWars/CodeWars/8kyu/GenerateRangeOfIntegers.cs
@@ -9,4 +9,17 @@ namespace CodeWars._8kyu
         public static int[] GenerateRange(int min, int max, int step)
         => Enumerable.Range(0, 1 + (max - min) / step).Select(i => min + i * step).ToArray();
     }
+
+    /// Instructions
+    ///
+    /// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max,
+    /// with the step.The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min<max)
+    /// Task
+    ///     Implement a function named
+    ///     GenerateRange(2, 10, 2) == new int[]{ 2, 4, 6, 8, 10 }
+    ///     GenerateRange(1, 10, 3) == new int[]{ 1, 4, 7, 10 }
+    /// Note
+    ///     min<max
+    ///     step > 0
+    ///     the range does not HAVE to include max (depending on the step)
 }
--- CodeWars/CodeWarsTests/8kyu/GenerateRangeOfIntegersTests.cs
+++ CodeWars/CodeWarsTests/8kyu/GenerateRangeOfIntegersTests.cs
@@ -20,16 +20,4 @@ namespace CodeWarsTests._8kyu
         }
     }
 
-    ///
-    /// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max,
-    /// with the step.The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min<max)
-    /// Task
-    /// Implement a function named
-    /// GenerateRange(2, 10, 2) == new int[]{ 2, 4, 6, 8, 10 }
-    /// GenerateRange(1, 10, 3) == new int[]{ 1, 4, 7, 10 }
-    /// Note
-    ///    min<max
-    ///    step > 0
-    ///    the range does not HAVE to include max (depending on the step)
-
 }
